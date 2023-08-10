02fc09d2b5e637988880d31bfcd726b6e53b6991
Aleeza <alxzmj469@gmail.com>
2023-08-10 14:11:34 +0500

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>AlSalaam</title>
+        <meta name="description" content="AlSalaam" />
+      </Head>
+      <div className="text-[red] flex justify-center items-center h-full">Coming Soon</div>
+    </>
+  );
+};
+
+export default Home;
