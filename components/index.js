b3c31f81b8483145e19a3dd3c707cf05e40fa761
Aleeza <alxzmj469@gmail.com>
2023-08-11 13:42:24 +0500

    fix import issue

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 import Hero from './hero';
-import RecommendationSection from ' ./recommendationSection';
+import RecommendationSection from './recommendationSection';
 import TopDeals from './topDeals';
 import TopDestinations from './topDestinations';
 
